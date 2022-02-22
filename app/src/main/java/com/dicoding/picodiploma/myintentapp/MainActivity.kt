a8d48668d2349d81e4d050b1dd78c69d62939294
Zaskha <zaskhas@gmail.com>
2022-02-23 05:17:17 +0700

    latihan explicit intent - membuat dan menjalankan tombol pindah activity

--- app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.dicoding.picodiploma.myintentapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        memperkenalkan button yang sudah ditambahkan
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+//        menambah event
+        btnMoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+//                intent
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+        }
     }
 }
