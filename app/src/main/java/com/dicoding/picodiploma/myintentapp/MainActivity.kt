ad38649be17f71cba61cd40538d54316300e1aa4
Zaskha <zaskhas@gmail.com>
2022-02-23 14:32:11 +0700

    latihan mengirim data pada intent - pindah activity dengan membawa data

--- app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
 //        menambah event
         btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -23,6 +26,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //                intent
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Zaskha Sasmita")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
+                startActivity(moveWithDataIntent)
             }
         }
     }
